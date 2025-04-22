5f05fa978234eced783cf8860f348876e31afa8a
johnnybravo <ezeufohchibuzor@gmail.com>
2025-04-22 21:19:04 +0100

    a dropdown menu to show current user

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation';
 import classNames from 'classnames';
 import { text } from 'body-parser';
 import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const Navbar = () => {
     const currentPath = usePathname()
@@ -38,7 +38,29 @@ const Navbar = () => {
                         </ul>
                     </Flex>
                     <Box>
-                        {status === "authenticated" && <Link href="/api/auth/signout">Logout</Link>}
+                        {status === "authenticated" &&
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar
+                                        src={session.user!.image!}
+                                        fallback="?"
+                                        size="2"
+                                        radius="full"
+                                        className="cursor-pointer"
+                                    />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text size="2">
+                                            {session.user!.email}
+                                        </Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Item>
+                                        <Link href="/api/auth/signout">Logout</Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                        }
                         {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
                     </Box>
                 </Flex>
