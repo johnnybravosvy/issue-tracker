ce9291c9f8ec741ad031ceee6da9e60522205ac6
johnnybravo <ezeufohchibuzor@gmail.com>
2025-04-25 14:35:56 +0100

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,46 +1,65 @@
-
-import React from 'react'
+import React from "react";
 import { Table } from "@radix-ui/themes";
-import { IssueStatusBadge, Link } from "@/app/components"
-import prisma from '@/prisma/client'
-import IssueToolbar from './IssueToolbar';
+import { IssueStatusBadge, Link } from "@/app/components";
+import prisma from "@/prisma/client";
+import IssueToolbar from "./IssueToolbar";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-    const issues = await prisma.issue.findMany()
-
-    return (
-        <div>
-            <IssueToolbar />
-            <Table.Root variant="surface">
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {issues.map(issue => (
-                        <Table.Row key={issue.id}>
-                            <Table.Cell>
-                                <Link href={`/issues/${issue.id}`}>
-                                    {issue.title}
-                                </Link>
-                                <div className="block md:hidden">
-                                    <IssueStatusBadge status={issue.status} />
-                                </div>
-                            </Table.Cell>
-                            <Table.Cell className="hidden md:table-cell"><IssueStatusBadge status={issue.status} /></Table.Cell>
-                            <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
-        </div>
-    )
+interface Props {
+  searchParams: { status: Status };
 }
 
-// export const revalidate = 0 // the same as setting dynamic to forcr dynamic, the output of this page has to be revalidate every 0 seconds
-export const dynamic = 'force-dynamic';
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
 
-export default IssuesPage
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
+
+  return (
+    <div>
+      <IssueToolbar />
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+
+// export const revalidate = 0 // the same as setting dynamic to forcr dynamic, the output of this page has to be revalidate every 0 seconds
+export const dynamic = "force-dynamic";
+
+export default IssuesPage;
